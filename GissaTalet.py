82493b422f4722e6a699c4992529c9330e5cca6a
Lowe Nordstrand <lowe.nordstrand@skola.taby.se>
2025-10-23 13:00:56 +0000

    Del 2: Slumpat tal och gissning

--- GissaTalet.py
+++ GissaTalet.py
@@ -1,2 +1,10 @@
+import random as rand
+
 namn = input("Vad heter du? ")
 input(f"Hej {namn} och välkommen till spelet!")
+slumptal = rand.randint(1, 10)
+tal = input("Gissa ett tal mellan 1 och 10: ")
+if tal == slumptal:
+    print("Du gissade rätt!")
+else:
+    print(f"Tyvärr, det korrekta svaret var {slumptal}")
